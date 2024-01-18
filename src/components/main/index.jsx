f930c798c69312a6b3cd6e1394b91e6327538851
Moiseis Cruz <moizzito@gmail.com>
2024-01-17 21:48:25 -0300

    I made the function to consume the API.

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,3 +1,12 @@
+async function getDatos(){
+    const response = await fetch("https://swapi.dev/api/people");
+    const data = await response.json();
+    console.log(data);
+    return data
+}
+
+getDatos()
+
 export const Main = () => {
     return(
         <section>

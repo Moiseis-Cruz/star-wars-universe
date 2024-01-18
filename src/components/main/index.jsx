5e5241a01095f181bad17440681397e1dd35a268
Moiseis Cruz <moizzito@gmail.com>
2024-01-18 12:28:52 -0300

    Imported state hook

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,8 +1,15 @@
+import { useState, useEffect } from "react"
+
 import { getDatos } from "../../service"
 
 getDatos()
 
 export const Main = () => {
+
+    const [ characters, setCharacters ] = useState({
+        ListCharacters : []
+    })
+
     return(
         <section>
             <h1>Teste</h1>

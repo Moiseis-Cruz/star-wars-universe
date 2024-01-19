9a20ecd2b006b8ed6b4cb92505fc6923be43412c
Moiseis Cruz <moizzito@gmail.com>
2024-01-18 21:12:13 -0300

    I created the Person function and component.

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -24,20 +24,26 @@ export const Main = () => {
 
     return(
         <section>
-            <ul>
-                {
-                    characters.listCharacters.map((item, index) => {
-                        return(
-                            <li key={index}>
-                                <div>
-                                    <h2>{item.name}</h2>
-                                    <p>{item.url}</p>
-                                </div>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+            <Person people={characters.listCharacters} />
         </section>
+    )
+}
+
+const Person = (props) => {
+    return(
+        <ul>
+            {
+                props.people.map((item, index) => {
+                    return(
+                        <li key={index}>
+                            <div>
+                                <h2>{item.name}</h2>
+                                <p>{item.url}</p>
+                            </div>
+                        </li>
+                    )
+                })
+            }
+        </ul>
     )
 }
